075c2aefd38d4e8b8d0d22850ad2131abd414f53
Luiserebii <luis@serebii.io>
2019-11-17 00:12:05 -0500

    Adding structuring test for CMakeLists.txt

---
+++ examples/cmake/structuring/main.cpp
@@ -0,0 +1,44 @@
+#include "../../../include/mint/mint.h"
+
+int main() {
+
+    //Declare and intialize Mint
+    Mint m;
+
+    //Title
+    m.title("Testing Operators");
+
+    //Test unit - int
+    m.minititle("int");
+
+    m.equal(0, 0, "=");
+    m.assert(0 != 1, "!=");
+    m.assert(0 < 1, "<");
+    m.assert(1 > 0, ">");
+    m.assert(0 >= 0, ">=");
+    m.assert(0 <= 0, "<=");
+
+    //Test unit - double
+    m.minititle("double");
+
+    m.equal(0.0, 0.0, "=");
+    m.assert(0.0 != 1.0, "!=");
+    m.assert(0.0 < 1.0, "<");
+    m.assert(1.0 > 0.0, ">");
+    m.assert(0.0 >= 0.0, ">=");
+    m.assert(0.0 <= 0.0, "<=");
+
+    //Test unit - char
+    m.minititle("char");
+
+    m.equal('a', 'a', "=");
+    m.assert('a' != 'b', "!=");
+    m.assert('a' < 'b', "<");
+    m.assert('b' > 'a', ">");
+    m.assert('a' >= 'a', ">=");
+    m.assert('a' <= 'a', "<=");
+
+
+    //Finish tests, and return exit code
+    return m.end();
+}
